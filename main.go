96f58ce29e59ca053a8a03ac94208532d7d8c3be
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-09-14 15:47:41 +0530

    reading from db

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"time"
+)
+
+func main() {
+	var (
+		id   int
+		name string
+	)
+	fmt.Println("mahesh")
+	db, err := sql.Open("postgres", "host=localhost dbname=dealscore sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("conected, time is %s", time.Now())
+	rows, err := db.Query("SELECT id, name from user_details where id =$1", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("success")
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	db.Close()
+}
