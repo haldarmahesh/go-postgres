87a16ff790ecf2ea8c72de1630fd9339476a6416
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-09-17 09:58:03 +0530

    global var and read config file

--- database/database.go
+++ database/database.go
@@ -2,16 +2,29 @@ package database
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "io"
+	_ "io/ioutil"
+	_ "log"
+	"os"
 )
 
+type Config struct {
+	adapter string `json:"adapter"`
+	host    string `json:"host"`
+	dbname  string `json:"dbname"`
+	sslmode string `json:"sslmode"`
+}
+
 func CreateConnection() *sql.DB {
-	db, err := sql.Open("postgres", "host=localhost dbname=delscore sslmode=disable")
+	db, err := sql.Open("postgres", "host=localhost dbname=dealscore sslmode=disable")
 	logError(err)
 	return db
 }
 
 func logError(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
--- main.go
+++ main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-  "fmt"
-  _ "github.com/haldarmahesh/string"
-  _ "github.com/lib/pq"
-  _ "log"
+	"database/sql"
+	"fmt"
+
+	"github.com/haldarmahesh/dbconnect/database"
+	_ "github.com/haldarmahesh/string"
+	_ "github.com/lib/pq"
+	_ "log"
 )
 
-import "github.com/haldarmahesh/dbconnect/database"
-func main() {
-  var (
-    id   int
-    name string
-  )
-  db := database.CreateConnection()
-  rows, err := db.Query("SELECT id, name from user_details where id=$1", 1)
-  logError(err)
-  defer rows.Close()
-  for rows.Next() {
-    err := rows.Scan(&id, &name)
-    logError(err)
-    fmt.Println(id, name)
-  }
-  db.Close()
+var db *sql.DB
 
-  
+func main() {
+	var (
+		id   int
+		name string
+	)
+	db = database.CreateConnection()
+	rows := readRows()
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		logError(err)
+		fmt.Println(id, name)
+	}
+	db.Close()
 }
 
 func logError(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
+}
+func readRows() *sql.Rows {
+	rows, err := db.Query("SELECT id, name from user_details where id=$1", 1)
+	logError(err)
+	return rows
 }
