ecf1b5878b4c56b73612ea2fac6fca8cab6b7e94
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-09-18 14:02:23 +0530

    read db params from config json

--- database/database.go
+++ database/database.go
@@ -3,10 +3,8 @@ package database
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	_ "log"
-	_ "os"
 	"path"
 	"path/filepath"
 	"runtime"
@@ -24,7 +22,7 @@ func CreateConnection() *sql.DB {
 	configuration := Config{}
 	err := json.Unmarshal(data, &configuration)
 	logError(err)
-	db, err := sql.Open("postgres", "host=localhost dbname=dealscore sslmode=disable")
+	db, err := sql.Open(configuration.Adapter, "host="+configuration.Host+" dbname="+configuration.Dbname+" sslmode="+configuration.Sslmode)
 	logError(err)
 	return db
 }
@@ -34,9 +32,9 @@ func logError(err error) {
 		panic(err)
 	}
 }
+
 func readJsonConfig() []byte {
 	_, filename1, _, _ := runtime.Caller(1)
-	fmt.Println(path.Dir(filename1), "$$")
 	filename, _ := filepath.Abs(path.Dir(filename1) + "/config.json")
 	jsonFile, err := ioutil.ReadFile(filename)
 	logError(err)
